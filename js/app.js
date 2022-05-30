581cf38f85bb7e5c13d28d17839b4cad0676b389
vanes2k <73784650+vanes2k@users.noreply.github.com>
2022-05-30 17:21:54 +0300

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,69 @@
+/* Theme Name:  Kasy- Responsive Landing page template
+  File Description: Main JS file of the template
+*/
+
+
+//  Window scroll sticky class add
+function windowScroll() {
+  const navbar = document.getElementById("navbar");
+  if (
+      document.body.scrollTop >= 50 ||
+      document.documentElement.scrollTop >= 50
+  ) {
+      navbar.classList.add("nav-sticky");
+  } else {
+      navbar.classList.remove("nav-sticky");
+  }
+}
+
+window.addEventListener('scroll', (ev) => {
+  ev.preventDefault();
+  windowScroll();
+})
+
+// Swiper slider
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+
+
+//
+/********************* scroll top js ************************/
+//
+
+var mybutton = document.getElementById("back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
